a948449a8147ba28898a67bc59eb357ca82f4e02
Ian Edwards <57515741+ianedwards-jpg@users.noreply.github.com>
2020-05-19 21:27:09 -0400

    updated other files

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,34 +1,46 @@
 //Place API call.. Takes in zip code and spits out postal code place ID
-var placeURL =
-  "https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=market&types=establishment&location=37.76999,-122.44696&radius=500&key=AIzaSyCBMsGzEjyXZdesMU6K8WJrJco58Q0R674";
+// var placeURL =
+//   "https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=market&types=establishment&location=37.76999,-122.44696&radius=500&key=AIzaSyCBMsGzEjyXZdesMU6K8WJrJco58Q0R674";
 
-  "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters"
+//   "https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters"
 
-$.ajax({
-  url: placeURL,
-  method: "GET"
-}).then(function(response) {
-  //for (var i = 0; i < 5; i++){
-  //console.log(response.restaurants[i].restaurant.user_rating.aggregate_rating);
-  //console.log(response.restaurants[i].restaurant.timings);
-  //console.log(response.restaurants[i].restaurant.location.locality);
-  console.log(response);
-  //console.log(response.results[i].title)
-  //}
-});
+// $.ajax({
+//   url: placeURL,
+//   method: "GET"
+// }).then(function(response) {
+//   //for (var i = 0; i < 5; i++){
+//   //console.log(response.restaurants[i].restaurant.user_rating.aggregate_rating);
+//   //console.log(response.restaurants[i].restaurant.timings);
+//   //console.log(response.restaurants[i].restaurant.location.locality);
+//   console.log(response);
+//   //console.log(response.results[i].title)
+//   //}
+// });
+function searchLocation(zipCode) {
+  var key = "&key=AIzaSyAtPSsLK6vd2cPiHSxryHM3OK98Kmenak4";
+  var superMarketURL =
+    "https://maps.googleapis.com/maps/api/place/textsearch/json?query=supermarkets+near+" +
+    zipCode +
+    key;
 
-var superMarketURL =
-  "https://maps.googleapis.com/maps/api/place/autocomplete/xml?input=market&types=establishment&location=37.76999,-122.44696&radius=500&key=AIzaSyCBMsGzEjyXZdesMU6K8WJrJco58Q0R674";
+  $.ajax({
+    url: superMarketURL,
+    method: "GET"
+  }).then(function(response) {
+    for (var i = 0; i < 5; i++) {
+      console.log(response.results[i].name);
+      console.log(response.results[i].opening_hours);
+      console.log(response.results[i].formatted_address);
+      console.log(response);
+    }
+  });
+}
 
-$.ajax({
-  url: superMarketURL,
-  method: "GET"
-}).then(function(response) {
-  //for (var i = 0; i < 5; i++){
-  //console.log(response.restaurants[i].restaurant.user_rating.aggregate_rating);
-  //console.log(response.restaurants[i].restaurant.timings);
-  //console.log(response.restaurants[i].restaurant.location.locality);
-  console.log(response);
-  //console.log(response.results[i].title)
-  //}
+$("#SearchBtn").on("click", function(event) {
+  event.preventDefault();
+  var cityinput = $("#Search-Term")
+    .val()
+    .trim();
+  searchLocation(cityinput);
+  $("#card-body1").empty();
 });
